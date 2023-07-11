65737a59cc064e5b0ec535e87c0e513c43f4fa16
Łukasz <lukaszkogut1996@gmail.com>
2023-07-11 21:18:36 +0200

    +

--- src/components/store.js
+++
@@ -1,6 +0,0 @@
-import { Store } from 'redux';
-import rootReducer from './reducers'; // Zaimportuj swój główny reducer (lub kombinację reducerów)
-
-const store = Store(rootReducer);
-
-export default store;
